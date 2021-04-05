d31e7d0f55e78c5c06188a9734acfe590e09f337
Kaitlyn Mercado <kristine.mercado@obf.ateneo.edu>
2021-04-05 18:12:01 +0800

    Updated Homepage to create new questboard and view existing questboard
    Combined code created by Andrea Pilapil and Kaitlyn Mercado

--- questboard/forms.py
+++ questboard/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
-class CreateForm(forms.Form):
-    name = forms.CharField(max_length=20, label='Name:')
-    description = forms.CharField(widget=forms.Textarea, label='Description:')
-    rstars = forms.ChoiceField(choices=[(x, x) for x in range(1, 6)], label='Required Stars:')
+
+from .models import *
+
+
+class QuestBoardForm(forms.ModelForm):
+	class Meta:
+		model = QuestBoard
+		fields = [
+			'name',
+			'description',
+			'required_stars',
+		]
--- questboard/views.py
+++ questboard/views.py
@@ -4,6 +4,20 @@ from django.views import View
 
 from .forms import *
 
+from .models import *
+
 def homepage_view(request):
-	form = CreateForm()
-	return render(request, "homepage.html", {"form":CreateForm})
+	form = QuestBoardForm(request.POST or None)
+	qb_count = QuestBoard.objects.all().count()
+	qb_id = list(QuestBoard.objects.values_list('name', flat=True).distinct())
+	if request.method == 'POST':
+		if form.is_valid():
+			new_questboard = form.save()
+			questboard_id = new_questboard.id
+			obj = QuestBoard.objects.get(id=questboard_id)
+			form = QuestBoardForm()
+	context = {
+			'form' : form,
+			'object' : obj,
+		}
+	return render(request, "homepage.html", context)
